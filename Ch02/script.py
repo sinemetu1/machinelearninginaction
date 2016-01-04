8e79269fdfca257f59cf23d3be4a8ea7d440a572
Sam Garrett <samdgarrett@gmail.com>
2016-01-03 19:23:46 -0500

    finished chapter 02

---
+++ Ch02/script.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+import kNN
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import array
+
+#kNN.datingClassTest()
+#kNN.classifyPerson()
+
+kNN.handwritingClassTest()
